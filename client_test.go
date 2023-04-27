e0b9bbebdc8aaec208d02876b13a6cbdb4a7780a
James Lindstorff <james.lindstorff@jppol.dk>
2023-04-27 08:28:54 +0200

    test props for vscode

--- client_test.go
+++ client_test.go
@@ -57,14 +57,10 @@ func TestShowServersState(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed show state : %v", err)
 	}
-	log.Printf("%+v", resp)
-}
+	//log.Printf("%+v", resp)
 
-/*
-1
-# be_id be_name srv_id srv_name srv_addr srv_op_state srv_admin_state srv_uweight srv_iweight srv_time_since_last_change srv_check_status srv_check_result srv_check_health srv_check_state srv_agent_state bk_f_forced_id srv_f_forced_id srv_fqdn srv_port srvrecord srv_use_ssl srv_check_port srv_check_addr srv_agent_addr srv_agent_port
-4 indexws 1 iws01 172.24.21.40 2 0 1 1 96 15 3 4 6 0 0 0 - 8080 - 0 0 - - 0
-4 indexws 2 iws02 172.24.21.32 2 0 1 1 96 15 3 4 6 0 0 0 - 8080 - 0 0 - - 0
-4 indexws 3 iws03 172.24.21.33 2 0 1 1 96 15 3 4 6 0 0 0 - 8080 - 0 0 - - 0
-5 solr 1 solr01 192.168.220.4 2 0 1 1 83 15 0 4 7 0 0 0 solr 8983 - 0 0 - - 0
-*/
+	for _, state := range resp {
+		log.Printf("%s/%s %d %d", state.BeName, state.SrvName, state.SrvAdminState, state.SrvOpState)
+	}
+
+}
